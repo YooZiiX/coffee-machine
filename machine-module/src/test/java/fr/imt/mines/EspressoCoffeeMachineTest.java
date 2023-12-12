c6c7fb98b821f06dba4641e04577c7992688f796
YooZiiX <jeremy.lucas@mines-ales.org>
2023-12-12 10:57:31 +0100

    Correction erreur : EspressoCoffeeMachineTest -> Expected not null

--- machine-module/src/test/java/fr/imt/mines/EspressoCoffeeMachineTest.java
+++ machine-module/src/test/java/fr/imt/mines/EspressoCoffeeMachineTest.java
@@ -16,16 +16,18 @@ import static org.junit.jupiter.api.Assertions.*;
 public class EspressoCoffeeMachineTest {
 
     private EspressoCoffeeMachine espressoCoffeeMachine;
+
     @BeforeEach
     void setUp() {
         espressoCoffeeMachine = new EspressoCoffeeMachine(10.0, 50.0, 5.0, 20.0, 2.0);
     }
+
     @Test
     void testMakeCoffeePlugged() {
         espressoCoffeeMachine.plugToElectricalPlug();
         espressoCoffeeMachine.addWaterInTank(10.0);
         espressoCoffeeMachine.addCoffeeInBeanTank(0.2, CoffeeType.ARABICA);
-        CoffeeContainer result = null;
+        CoffeeContainer result;
         try {
             result = espressoCoffeeMachine.makeACoffee(new Cup(0.2), CoffeeType.ARABICA);
         } catch (LackOfWaterInTankException | InterruptedException | MachineNotPluggedException | CupNotEmptyException |
